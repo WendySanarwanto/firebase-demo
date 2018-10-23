3929adbfb18418ce190b3a71cd13d7a14e5839ba
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-10-23 17:15:03 +0800

    Updated the current implementation as an Express app

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,21 @@
+import * as express from 'express';
 import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
 
+import { ItemsRouter } from './items';
+
+// Initialise Firebase app & suppress a default warning when accessing firestore later.
 admin.initializeApp();    
 const firestore: admin.firestore.Firestore = admin.firestore();
 firestore.settings({ timestampsInSnapshots: true });
 
-export * from './items/item.routes';
+// Create express application 
+const app = express();
+
+// TODO: Initialise required express middlewares
+
+// TODO: Add more required routes
+app.use(`/items`, ItemsRouter);
+
+// Expose the Express API Routes as functions
+export const api = functions.https.onRequest(app);
--- functions/src/items/item.routes.ts
+++ functions/src/items/item.routes.ts
@@ -1,12 +1,18 @@
-import * as functions from 'firebase-functions';
-
+// import * as functions from 'firebase-functions';
+import * as express from 'express';
 import { ItemService } from './item.service';
 
-export const addItem = functions.https.onRequest(async (req, res) => {
-  // Grab the request's body
-  // console.log(`[DEBUG] - <addItem> req.body: \n`, req.body);
-  const itemService: ItemService = new ItemService();
-  const response = await itemService.addItem(req.body);
-  console.log(`[DEBUG] - <addItem> response: \n`, response);
-  res.json(response);
+export const ItemsRouter = express.Router();
+ItemsRouter.post("/", async (req, res) => {
+  try{
+    // Grab the request's body
+    console.log(`[DEBUG] - <items.routes.addItem> req.body: \n`, req.body);
+    const itemService: ItemService = new ItemService();
+    const response = await itemService.addItem(req.body);
+    console.log(`[DEBUG] - <items.routes.addItem> response: \n`, response);
+    res.status(201).json(response);
+  } catch(error) {
+    console.log('[ERROR] - <items.routes.addItem>. Details: \n', error);
+    res.sendStatus(500);
+  }
 });
