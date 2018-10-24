d1c205bbe01c18c0fd43cae066260c91d20ca22d
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2018-10-25 07:27:56 +0800

    Added @angular-mdl/core dependency

--- clients/sign-in-app/src/app/app.module.ts
+++ clients/sign-in-app/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MdlModule } from '@angular-mdl/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    MdlModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
