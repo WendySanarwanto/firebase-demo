9ce1b477b97caaa441b778a32a22a1c326136249
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-10-25 13:21:21 +0800

    Added isLoged  method in sign i ncard component

--- clients/sign-in-app/src/app/sign-in-card/sign-in-card.component.ts
+++ clients/sign-in-app/src/app/sign-in-card/sign-in-card.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../services';
+
 @Component({
   selector: 'app-sign-in-card',
   templateUrl: './sign-in-card.component.html',
   styleUrls: ['./sign-in-card.component.css']
 })
 export class SignInCardComponent implements OnInit {
+  // TODO: Inject Auth service on the constructor's arg
+  constructor(private _auth: AuthService) { }
 
-  constructor() { }
+  ngOnInit() { }
 
-  ngOnInit() {
+  isLoggedIn() {
+    // TODO: Call auth service's isLogged in to determine whether the user is authenticated or not
+    return this._auth.isLoggedIn();
   }
 
+  onSignInClicked() {
+    alert(`Not implemented !`);
+  }
 }
